7e5e6e851e6f0d8c0ea8f85f9d81c66e23b2aa15
Jiwon Lee <giwon.yi339@gmail.com>
2024-09-27 14:29:45 +0900

    feat: query invalidation added upon successful update

--- src/hook/usePenalizeGameHook.tsx
+++ src/hook/usePenalizeGameHook.tsx
@@ -1,7 +1,9 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { penalizeGame } from "../api/reports";
 
 export const usePenalizeGameHook = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: ({
       gameId,
@@ -10,5 +12,10 @@ export const usePenalizeGameHook = () => {
       gameId: number | null;
       data: { penalty: string; duration: number | null };
     }) => penalizeGame(gameId, data),
+    onSuccess: () => {
+      //   queryClient.invalidateQueries("Users");
+      queryClient.invalidateQueries({ queryKey: ["Reports list"] });
+      queryClient.invalidateQueries({ queryKey: ["Reporters List"] });
+    },
   });
 };
