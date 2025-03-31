3580be8b7f9e406befc400271b8f0e02bc8a41ff
Jiwon Lee <giwon.yi339@gmail.com>
2025-03-31 23:01:50 +0900

    feat: v1.2 user list, user detail page created with rendered data

--- src/pages/users/UserDetails.tsx
+++ src/pages/users/UserDetails.tsx
@@ -1,5 +1,6 @@
 import { PageLayout } from "../../features/common/PageLayout";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useGetUserDetails } from "../../features/users/hooks/useGetUserDetails";
+import { useParams } from "react-router-dom";
 /**
  * 
  * 렌더링 정보 : id, 이메일, 닉네임, 이름, 생년월일, 가입수단, 연락처, 프로필 이미지, 가입일시, 
@@ -8,28 +9,37 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 회원 정지
  */
 export const UserDetails = () => {
+  const { userId } = useParams();
+  console.log(userId);
+  const { data } = useGetUserDetails(Number(userId));
+
+  const userData = data?.data;
   return (
     <PageLayout>
       <div className="   p-4 flex flex-col items-center justify-center gap-8 ">
         {" "}
         <div className="flex flex-col items-center gap-2">
-          <AccountCircleIcon sx={{ fontSize: 60 }} />
-          (123)
+          {/* <AccountCircleIcon sx={{ fontSize: 60 }} /> */}
+          <img
+            src={userData?.profile_image_url}
+            alt="profile avatar"
+            className="size-36 rounded-full"
+          />
         </div>
         <hr className="h-1 w-full bg-gray-200 rounded-xl" />
         <div className="flex gap-20  w-full px-[10rem]">
           {" "}
           <h1 className="font-bold text-lg w-[8rem]">사용자 정보</h1>
           <ul className="flex flex-col gap-2">
-            <li>이름 : 이지원 </li>
-            <li>이메일 : miti@makeittakeit.kr </li>
-            <li>연락처 : 미티관리자 </li>
-            <li>닉네임 : 미티관리자 </li>
-            <li>생년월일 : 1996.05.19</li>
+            <li>이름 : {userData?.name} </li>
+            <li>이메일 : {userData?.email} </li>
+            <li>연락처 : {userData?.phone} </li>
+            <li>닉네임 : {userData?.nickname} </li>
+            <li>생년월일 : {userData?.birthday}</li>
           </ul>
-          <ul>
-            <li>가입수단 : kakao</li>
-            <li>가입일시 : 2024.12.19</li>
+          <ul className="space-y-2">
+            <li>가입수단 : {userData?.signup_method}</li>
+            <li>가입일시 : {userData?.created_at.slice(0, 10)}</li>
           </ul>
         </div>
         <hr className="h-1 w-full bg-gray-200 rounded-xl" />
@@ -37,11 +47,11 @@ export const UserDetails = () => {
           <h1 className="font-bold text-lg w-[8rem]">플레이어 프로필</h1>
           {/* 플레이어 프로필(성별, 체중, 신장, 포지션, 역할) */}
           <ul className="flex flex-col gap-2">
-            <li>성별 : 남 </li>
-            <li>체중 : 78kg </li>
-            <li>신장 : 182cm </li>
-            <li>포지션 : 스몰포워드</li>
-            <li>역할 : 가드</li>
+            <li>성별 : {userData?.player_profile.gender} </li>
+            <li>체중 : {userData?.player_profile.weight} </li>
+            <li>신장 : {userData?.player_profile.height} </li>
+            <li>포지션 : {userData?.player_profile.position}</li>
+            <li>역할 : {userData?.player_profile.role}</li>
           </ul>
         </div>
       </div>
--- src/pages/users/UserList.tsx
+++ src/pages/users/UserList.tsx
@@ -1,99 +1,83 @@
-import { useEffect, useState } from "react";
-import { useUserStore } from "../../store/useUserStore";
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useUsersListHook } from "../../hook/useUsersListHook";
 import PaginationBtns from "../../components/common/PaginationBtns";
 
 import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 import { PageLayout } from "../../features/common/PageLayout";
+import { PageHeader } from "../../features/common/PageHeader";
 const UserList = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
-
-  const { isLoggedIn, logout } = useUserStore();
-  const navigate = useNavigate();
 
   const { data, isLoading } = useUsersListHook(currentPage);
 
   const endIndex = data?.data.end_index;
   const userData = data?.data.page_content;
 
-  const userSessionStorage = sessionStorage.getItem("accessToken");
-
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate("/");
-    }
-
-    if (!userSessionStorage) {
-      logout();
-    }
-  }, [navigate, isLoggedIn, logout, userSessionStorage]);
-
   return (
-    <PageLayout>
-      <div className="flex flex-col gap-2">
-        <ul className="flex w-full  items-center  pt-2 pb-4">
-          <li className="font-bold  w-[10%] text-center ">ID</li>
-          <li className="font-bold  w-[10%] text-center ">가입수단</li>
-          <li className="font-bold  w-[15%] text-center ">닉네임</li>
-          <li className="font-bold  w-[25%] text-center ">이메일</li>
-          <li className="font-bold  w-[10%] text-center ">생년월일</li>
-          <li className="font-bold  w-[20%] text-center ">연락처</li>
-          <li className="font-bold  w-[10%] text-center ">상세</li>
-        </ul>
-        <hr />
-        <div className="flex flex-col gap-2 py-4">
-          {!isLoading && data?.status_code === 200 ? (
-            userData?.map(
-              (user: {
-                id: number;
-                signup_method: string;
-                nickname: string;
-                email: string;
-              }) => {
-                return (
-                  <ul className="flex w-full  items-center text-sm hover:bg-gray-200 h-[60px] ">
-                    <li className="w-[10%] text-center ">{user.id}</li>
-                    <li className="w-[10%] text-center ">
-                      {user.signup_method}
-                    </li>
-                    <li className="w-[15%] text-center truncate ">
-                      {user.nickname}
-                    </li>
-                    <li className="w-[25%] text-center  truncate">
-                      {user.email}
-                    </li>
+    <>
+      <PageHeader title="회원 목록" />
+      <PageLayout>
+        <div className="flex flex-col gap-2">
+          <ul className="flex w-full  items-center h-[4rem] bg-white">
+            <li className="font-bold  w-[10%] text-center ">ID</li>
+            <li className="font-bold  w-[10%] text-center ">가입수단</li>
+            <li className="font-bold  w-[15%] text-center ">닉네임</li>
+            <li className="font-bold  w-[25%] text-center ">이메일</li>
+            <li className="font-bold  w-[10%] text-center ">생년월일</li>
+            <li className="font-bold  w-[20%] text-center ">연락처</li>
+            <li className="font-bold  w-[10%] text-center ">상세</li>
+          </ul>
+          <hr />
+          <div className="flex flex-col gap-2 py-4 bg-white">
+            {!isLoading && data?.status_code === 200 ? (
+              userData?.map(
+                (user: {
+                  id: number;
+                  signup_method: string;
+                  nickname: string;
+                  email: string;
+                  birthday: string;
+                  phone: string;
+                }) => {
+                  return (
+                    <ul className="flex w-full  items-center text-sm hover:bg-gray-200 h-[60px] ">
+                      <li className="w-[10%] text-center ">{user.id}</li>
+                      <li className="w-[10%] text-center ">
+                        {user.signup_method}
+                      </li>
+                      <li className="w-[15%] text-center truncate ">
+                        {user.nickname}
+                      </li>
+                      <li className="w-[25%] text-center  truncate">
+                        {user.email}
+                      </li>
 
-                    <li className="w-[10%] text-center  ">1996.05.19</li>
-                    <li className="w-[20%] text-center  ">010-2584-0519</li>
-                    <button type="button">
-                      <Link
-                        to="123"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="w-[10%] text-center"
-                      >
+                      <li className="w-[10%] text-center  ">{user.birthday}</li>
+                      <li className="w-[20%] text-center  ">{user.phone}</li>
+
+                      <Link to={`${user.id}`} className="w-[10%] text-center">
                         <PersonSearchIcon />
                       </Link>
-                    </button>
-                  </ul>
-                );
-              }
-            )
-          ) : (
-            <h1 className="flex items-center justify-center font-bold">
-              오류 발생
-            </h1>
-          )}
+                    </ul>
+                  );
+                }
+              )
+            ) : (
+              <h1 className="flex items-center justify-center font-bold">
+                오류 발생
+              </h1>
+            )}
+          </div>
         </div>
-      </div>
-      <PaginationBtns
-        spacing={2}
-        count={endIndex}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-    </PageLayout>
+        <PaginationBtns
+          spacing={2}
+          count={endIndex}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </PageLayout>
+    </>
   );
 };
 
