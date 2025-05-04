16f227f897f6d95ba74787a3c332bd4178f05ca4
Jiwon Lee <giwon.yi339@gmail.com>
2025-05-04 11:24:30 +0900

    refactor: update user inquiry page name

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,14 +1,8 @@
-// import MenuIcon from "@mui/icons-material/Menu";
-
-import GroupIcon from "@mui/icons-material/Group";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import FlagIcon from "@mui/icons-material/Flag";
-import PaymentIcon from "@mui/icons-material/Payment";
-import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
-import SupportAgentIcon from "@mui/icons-material/SupportAgent";
-import ReceiptIcon from "@mui/icons-material/Receipt";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useUserStore } from "../../store/useUserStore";
+import { Button } from "../ui/button";
+import clsx from "clsx";
+import { NAVIGATION } from "../../constants/navigation.ts";
 
 const Navbar = () => {
   const { logout } = useUserStore();
@@ -22,63 +16,27 @@ const Navbar = () => {
 
   const location = useLocation();
 
-  const NAVIGATION = [
-    {
-      title: "대시보드",
-      path: "/dashboard",
-      icon: <DashboardIcon />,
-    },
-    { title: "회원 목록", path: "/users", icon: <GroupIcon /> },
-    {
-      title: "경기 목록",
-      path: "/games",
-      icon: <SportsBasketballIcon />,
-    },
-    {
-      title: "신고 목록",
-      path: "/reports",
-      icon: <FlagIcon />,
-    },
-
-    {
-      title: "정산금 요청 목록",
-      path: "/settlements",
-      icon: <PaymentIcon />,
-    },
-
-    {
-      title: "익명 문의 목록",
-      path: "/support",
-      icon: <SupportAgentIcon />,
-    },
-    {
-      title: "결제완료 목록",
-      path: "/payments",
-      icon: <ReceiptIcon />,
-    },
-  ];
-
   return (
     <nav
       className={` ${
         location.pathname === "/" || location.pathname === "/auth"
           ? "hidden"
           : "block"
-      }  h-[4rem] bg-[#000] text-white w-full flex items-center justify-between px-[2rem]  drop-b-shadow-sm  z-[9999]`}
+      }  h-[3.5rem] bg-[#000] text-white w-full flex items-center justify-between px-[2rem]  drop-b-shadow-sm  z-[9999]`}
     >
       <div className="flex items-center gap-6">
         <h1 className="font-bold text-2xl">MITI</h1>
-        <ul className="flex items-center  gap-4 font-[400] ">
+        <ul className="flex items-center gap-4 font-[400]">
           {NAVIGATION.map((nav, index) => (
             <li key={index}>
               <Link
-                className=" text-[16px] font-semibold "
+                className={clsx(
+                  "text-sm font-semibold",
+                  location.pathname.includes(nav.path)
+                    ? "text-white"
+                    : "text-[#717171]"
+                )}
                 to={nav.path}
-                style={{
-                  color: location.pathname.includes(nav.path)
-                    ? "#fff"
-                    : "#717171",
-                }}
               >
                 {nav.title}
               </Link>
@@ -87,9 +45,9 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <button type="button" className="hover:font-bold" onClick={handleLogout}>
-        로그아웃
-      </button>
+      <Button onClick={handleLogout} variant="ghost" size={"default"}>
+        <span className="text-[14x] font-semibold">로그아웃</span>
+      </Button>
     </nav>
   );
 };
