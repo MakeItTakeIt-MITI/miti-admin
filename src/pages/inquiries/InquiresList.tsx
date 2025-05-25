8134856ccda939cfe2ff5fe474c1ba160d7b2f02
Jiwon Lee <giwon.yi339@gmail.com>
2025-05-25 21:56:45 +0900

    refactor: user inquiry list page shadcn table ui update

--- src/pages/inquiries/InquiresList.tsx
+++ src/pages/inquiries/InquiresList.tsx
@@ -1,72 +1,165 @@
-import { useState } from "react";
-
-import PaginationBtns from "../../components/common/PaginationBtns";
 import { useInquiriesListHook } from "../../features/inquries/hooks/useInquiriesListHook";
-import FeedIcon from "@mui/icons-material/Feed";
-import { PageLayout } from "../../features/common/PageLayout";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { InquiryDataField } from "../../features/inquries/interface/inquries";
-import { TableLayout } from "../../components/common/TableLayout";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  Row,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../components/ui/table";
+import { NotepadText } from "lucide-react";
+import { PaginationWithLinks } from "../../components/common/PaginationWithLinks";
 
 const InquiresList = () => {
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get("page");
+  const pageNow = page ? parseInt(page) : 1;
 
-  const { data, isLoading } = useInquiriesListHook(currentPage);
+  const { data } = useInquiriesListHook(pageNow);
+  const currentPage = data?.data?.current_index;
 
   const endIndex = data?.data.end_index;
-  const privateInquriyData = data?.data.page_content;
 
-  const headers = [
-    "문의 ID",
-    "사용자 ID",
-    "제목",
-    "답변 개수",
-    "생성일",
-    "수정일",
-    "상세",
+  const columns: ColumnDef<InquiryDataField>[] = [
+    {
+      accessorKey: `id`,
+      header: `문의 ID`,
+    },
+    {
+      accessorKey: "user",
+      header: "사용자 ID",
+    },
+    {
+      accessorKey: "title",
+      header: "제목",
+    },
+    {
+      accessorKey: "num_of_answers",
+      header: "답변 갯수",
+    },
+    {
+      accessorKey: "created_at",
+      header: "생성일",
+    },
+
+    {
+      accessorKey: "modified_at",
+      header: "수정일",
+    },
+
+    {
+      accessorKey: "info",
+      header: "상세",
+      cell: ({ row }: { row: Row<InquiryDataField> }) => (
+        <Link to={`${row.original.id}`}>
+          <NotepadText />
+        </Link>
+      ),
+    },
   ];
-  const tableData =
-    !isLoading && data?.status_code === 200
-      ? privateInquriyData.map((inquiry: InquiryDataField) => [
-          inquiry.id,
-          inquiry.user,
-          inquiry.title,
-          inquiry.num_of_answers,
-          inquiry.created_at,
-          inquiry.modified_at,
-          <Link
-            to={`${inquiry.id}`}
-            className="inline-block w-full text-center"
-          >
-            <FeedIcon sx={{ color: "gray" }} />
-          </Link>,
-        ])
-      : [];
-
+  const table = useReactTable({
+    data: data?.data.page_content || [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    enableRowSelection: true,
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
   return (
-    <>
-      <PageLayout>
-        <div className="flex flex-col gap-2 ">
-          {data?.status_code === 200 ? (
-            <TableLayout
-              headers={headers}
-              data={tableData}
-              context="신고 내역이 없습니다!"
-            />
-          ) : (
-            <h1 className="flex items-center justify-center font-bold">
-              오류 발생
-            </h1>
-          )}
-        </div>
-        <PaginationBtns
-          spacing={2}
-          count={endIndex}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
-      </PageLayout>
-    </>
+    <section className="w-full  p-8  flex flex-col justify-between bg-black">
+      <div className="space-y-4">
+        <h1 className="text-white font-bold text-2xl">유저 문의 목록</h1>
+        <Table className=" overflow-y-auto  text-white">
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <PaginationWithLinks
+        page={currentPage}
+        pageSize={5}
+        totalCount={endIndex}
+      />
+    </section>
+    // <>
+    //   <PageLayout>
+    //     <div className="flex flex-col gap-2 ">
+    //       {data?.status_code === 200 ? (
+    //         <TableLayout
+    //           headers={headers}
+    //           data={tableData}
+    //           context="신고 내역이 없습니다!"
+    //         />
+    //       ) : (
+    //         <h1 className="flex items-center justify-center font-bold">
+    //           오류 발생
+    //         </h1>
+    //       )}
+    //     </div>
+    //     <PaginationBtns
+    //       spacing={2}
+    //       count={endIndex}
+    //       currentPage={currentPage}
+    //       setCurrentPage={setCurrentPage}
+    //     />
+    //   </PageLayout>
+    // </>
   );
 };
 
