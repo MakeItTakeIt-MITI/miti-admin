afa9d7ee886107ecbdcd401f8fdfdf8e2e98043b
Jiwon Lee <giwon.yi339@gmail.com>
2025-11-21 22:29:59 +0100

    refactor: remove payments tab

--- src/constants/NAVIGATION.ts
+++ src/constants/NAVIGATION.ts
@@ -3,7 +3,7 @@ import FlagIcon from "@mui/icons-material/Flag";
 import PaymentIcon from "@mui/icons-material/Payment";
 import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
 import SupportAgentIcon from "@mui/icons-material/SupportAgent";
-import ReceiptIcon from "@mui/icons-material/Receipt";
+// import ReceiptIcon from "@mui/icons-material/Receipt";
 
 export const NAVIGATION = [
     { title: "회원 목록", path: "/users?page=1&search=", icon: GroupIcon },
@@ -11,5 +11,5 @@ export const NAVIGATION = [
     { title: "신고 목록", path: "/reports?page=1&search=", icon: FlagIcon },
     { title: "정산금 요청 목록", path: "/settlements?page=1&search=", icon: PaymentIcon },
     { title: "유저 문의 목록", path: "/support?page=1&search=", icon: SupportAgentIcon },
-    { title: "결제완료 목록", path: "/payments?page=1&search=", icon: ReceiptIcon },
+    // { title: "결제완료 목록", path: "/payments?page=1&search=", icon: ReceiptIcon },
 ];
