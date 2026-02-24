4edb8750f7bca135f36d25b1b0f22ca6574d80b5
이지원 <giwon.yi339@gmail.com>
2026-02-24 16:35:25 +0900

    feat: toast alert added to court edit details and NCP image upload api

--- src/features/courts/hooks/mutation/useEditCourtDetails.tsx
+++ src/features/courts/hooks/mutation/useEditCourtDetails.tsx
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { patchCourtsDetails } from "../../api/courts";
+import { toast } from "react-toastify";
 
-// Patchable fields
 interface CourtPatchPayload {
   name?: string;
   info?: string;
@@ -17,9 +17,15 @@ export const useEditCourtDetails = (courtId: number) => {
       if (!courtId) throw new Error("courtId is required");
       return patchCourtsDetails(courtId, data);
     },
-    onSuccess: () => {
+    onSuccess: (res) => {
+      console.log("success", res);
+      toast.success("경기장 정보가 성공적으로 수정되었습니다.");
       queryClient.invalidateQueries({ queryKey: ["courtDetails", courtId] });
       queryClient.invalidateQueries({ queryKey: ["courtsList"] });
+    },
+    onError: (res) => {
+      console.log("error", res);
+      toast.error("경기장 정보 수정에 실패했습니다. 다시 시도해주세요.");
     },
   });
 };
--- src/features/courts/hooks/useCourtsDetailPage.tsx
+++ src/features/courts/hooks/useCourtsDetailPage.tsx
@@ -4,6 +4,7 @@ import { useCallback, useState } from "react";
 import { useEditCourtDetails } from "./mutation/useEditCourtDetails";
 import { useGetFileUrl } from "./query/useGetFileUrl";
 import { useUploadImage } from "./useUploadImage";
+import { toast } from "react-toastify";
 
 export const useCourtsDetailPage = () => {
   const [responseUploadUrl, setResponseUploadUrl] = useState<string[]>([]);
@@ -66,7 +67,7 @@ export const useCourtsDetailPage = () => {
 
   const { mutate: uploadImg, isPending: uploadImgPending } = useUploadImage(
     uploadUrl,
-    contentType
+    contentType,
   );
 
   const uploadImgToNaverHandler = useCallback(() => {
@@ -76,9 +77,14 @@ export const useCourtsDetailPage = () => {
         uploadImg(fileToUpload, {
           onSuccess: (data) => {
             const isSuccess = data.status === 200;
+
             if (isSuccess) {
               setResponseUploadUrl((prev) => [...prev, fileUrl]);
+              toast.success("NCP 이미지 업로드 성공");
             }
+          },
+          onError: () => {
+            toast.error("NCP 이미지 업로드 실패");
           },
         });
       }
