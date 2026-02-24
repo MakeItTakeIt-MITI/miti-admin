488f4149992eaa2a88305157c62f03a38b9bdcaf
이지원 <giwon.yi339@gmail.com>
2026-02-24 15:59:05 +0900

    feat: toast success/error alert added to private inquiry reply

--- src/features/private_inquries/hooks/mutation/useAddPrivateInquiryAnswer.tsx
+++ src/features/private_inquries/hooks/mutation/useAddPrivateInquiryAnswer.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postPrivateInquiresAnswer } from "../../api/private_inquriies";
+import { toast } from "react-toastify";
 
 export const useAddPrivateInquiryAnswer = (inquiryId: number) => {
   const queryClient = useQueryClient();
@@ -8,10 +9,16 @@ export const useAddPrivateInquiryAnswer = (inquiryId: number) => {
     mutationKey: ["addPrivateInquiryAnswer", inquiryId],
     mutationFn: ({ content }: { content: string }) =>
       postPrivateInquiresAnswer(inquiryId, { content }),
-    onSuccess: () => {
+    onSuccess: (res) => {
+      if (res.status_code === 201) {
+        toast.success("답변이 등록되었습니다.");
+      }
       queryClient.invalidateQueries({
         queryKey: ["privateInquiryAnswer", inquiryId],
       });
+    },
+    onError: () => {
+      toast.error("답변 등록에 실패했습니다.");
     },
   });
 };
